aa9dcddefc7b4159015ebdd244ea0e9d9efc5dfa
thanh.ro <thanhroda1@gmail.com>
2026-02-06 00:21:42 +0700

    Use name staitic factory method

--- src/main/java/com/roda/effectivejava/item01_static_factory/GoodUser.java
+++ src/main/java/com/roda/effectivejava/item01_static_factory/GoodUser.java
@@ -1,23 +1,52 @@
 package com.roda.effectivejava.item01_static_factory;
 
 /**
- * Sometimes is bad when there is more than one constructor with so many params
+ * Static constructor has name and easy to use
  */
-public class BadUser {
-    private String name;
-    private String first;
-    private String last;
-    private String old;
+public class GoodUser {
+    private final String name;
+    private final String firstName;
+    private final String lastName;
+    private final int age;
+    private final boolean active;
 
-    public BadUser(String name, String first, String last, String old) {
+    private GoodUser(String name, String firstName, String lastName, int age, boolean active) {
         this.name = name;
-        this.first = first;
-        this.last = last;
-        this.old = old;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.active = active;
     }
 
-    public BadUser(String name, String first) {
-        this.name = name;
-        this.first = first;
+    public static GoodUser activeAdmin(int age) {
+        return new GoodUser("ADMIN", null, null, age, true);
+    }
+
+    public static GoodUser withName(String name) {
+        return new GoodUser(name, "", "", 0, false);
+    }
+
+    public static GoodUser withNameAndFirstName(String name, String firstName) {
+        return new GoodUser(name, firstName, "", 0, false);
+    }
+
+    public static GoodUser fullUser(String name, String firstName, String lastName, int age) {
+        return new GoodUser(name, firstName, lastName, age, false);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
