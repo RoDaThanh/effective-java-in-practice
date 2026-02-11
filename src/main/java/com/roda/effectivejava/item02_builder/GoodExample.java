974bee36a51276998cb8445a93b9f3182666a44f
thanh.ro <thanhroda1@gmail.com>
2026-02-11 23:43:59 +0700

    Builder: using when there are many parameters and some of them are not required

---
+++ src/main/java/com/roda/effectivejava/item02_builder/GoodExample.java
@@ -0,0 +1,63 @@
+package com.roda.effectivejava.item02_builder;
+
+/**
+ * the telescoping constructor pattern works, but it is hard to write
+ * client code when there are many parameters, and harder still to read it
+ */
+public class GoodExample {
+    private final int size;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    private GoodExample(Builder builder) {
+        this.size = builder.size;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    public static class Builder {
+        private final int size;
+        private final int servings;
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int size, int servings) {
+            this.size = size;
+            this.servings = servings;
+        }
+
+        public Builder calories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder fat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder sodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder carbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public GoodExample build() {
+            return new GoodExample(this);
+        }
+    }
+}
+
+
